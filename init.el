a94dce592d369b8e63dfacd34c05ed47dbdcaf2e
udsquid <udsquid.altair@icloud.com>
2013-05-02 00:00:52 +0800

    Add procedure to load functions in 'defuns'

--- init.el
+++ init.el
@@ -83,6 +83,12 @@
 (require 'setup-paredit)
 (require 'setup-html-mode)
 
+;; Functions (load all files in defuns-dir)
+(setq defuns-dir (expand-file-name "defuns" user-emacs-directory))
+(dolist (file (directory-files defuns-dir t "\\w+"))
+  (when (file-regular-p file)
+    (load file)))
+
 ;; Load packages
 (require 'expand-region)
 (require 'visual-regexp)
