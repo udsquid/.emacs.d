4cb662498054f4c74bcb3549b9ecda5417c5bc36
udsquid <udsquid.altair@icloud.com>
2013-05-03 11:30:21 +0800

    Show full buffer path on title bar

--- appearance.el
+++ appearance.el
@@ -6,6 +6,11 @@
 ;; Highlight matching parentheses when the point is on them.
 (show-paren-mode 1)
 
+(when window-system
+  (setq frame-title-format '(buffer-file-name "%f" ("%b")))
+  (tooltip-mode -1)
+  (blink-cursor-mode -1))
+
 ;; Always display line and column numbers
 (setq line-number-mode t)
 (setq column-number-mode t)
