1058cf37cca915633d4f5148889d915c6ac4f5cd
Neil Lin <neil.lin@ruckuswireless.com>
2020-04-21 14:54:04 +0800

    Add comments

--- init.el
+++ init.el
@@ -1,8 +1,11 @@
+; set 'command' as META key, 'option/alt' as SUPER key
 (setq mac-command-modifier 'meta)
 (setq mac-option-modifier 'super)
 
+; switch 'C-x' & 'C-t' keys
 (keyboard-translate ?\C-x ?\C-t)
 (keyboard-translate ?\C-t ?\C-x)
 
+; quick switch between two recent buffers
 (fset 'quick-switch-buffer [?\C-x ?b return])
 (global-set-key (kbd "s-f") 'quick-switch-buffer)
