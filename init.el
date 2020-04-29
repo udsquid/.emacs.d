081c463e3cc82fc0af5f84751954f8f112b665cd
Neil Lin <neil.lin@ruckuswireless.com>
2020-04-29 15:31:18 +0800

    Add package repo & manager

--- init.el
+++ init.el
@@ -1,11 +1,38 @@
-; set 'command' as META key, 'option/alt' as SUPER key
+;; set 'command' as META key, 'option/alt' as SUPER key
 (setq mac-command-modifier 'meta)
 (setq mac-option-modifier 'super)
 
-; switch 'C-x' & 'C-t' keys
+;; switch 'C-x' & 'C-t' keys
 (keyboard-translate ?\C-x ?\C-t)
 (keyboard-translate ?\C-t ?\C-x)
 
-; quick switch between two recent buffers
+;; quick switch between two recent buffers
 (fset 'quick-switch-buffer [?\C-x ?b return])
 (global-set-key (kbd "s-f") 'quick-switch-buffer)
+
+;; setup package repository
+;; copy from: https://melpa.org/#/getting-started
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  (when no-ssl (warn "\
+Your version of Emacs does not support SSL connections,
+which is unsafe because it allows man-in-the-middle attacks.
+There are two things you can do about this warning:
+1. Install an Emacs version that does support SSL and be safe.
+2. Remove this warning from your init file so you won't see it again."))
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+  ;; and `package-pinned-packages`. Most users will not need or want to do this.
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  )
+(package-initialize)
+
+;; install 'use-package', a package to tidy up your package configurations
+(if (not (package-installed-p 'use-package))
+    (progn
+      (package-refresh-contents)
+      (package-install 'use-package)))
+
+(require 'use-package)
