7f42600e36aa7a9eac9bed6078c4cefec36cc66c
Neil Lin <neil.lin@ruckuswireless.com>
2020-04-29 15:35:46 +0800

    Setup theme

--- init.el
+++ init.el
@@ -36,3 +36,20 @@ There are two things you can do about this warning:
       (package-install 'use-package)))
 
 (require 'use-package)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (cyberpunk-theme use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; setup theme
+(use-package cyberpunk-theme
+  :ensure t)
