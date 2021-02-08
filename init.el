0c283f25906f29bb1d258198f0a822076d33a55c
neil.lin <neil.lin@ruckuswireless.com>
2021-02-08 11:09:51 +0800

    Fix typo

--- init.el
+++ init.el
@@ -188,7 +188,7 @@
   (require 'org-tempo)
   (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
   (add-to-list 'org-structure-template-alist '("sh" . "src shell"))
-  (add-to-list 'org-structure-template-alist '("py" . "src py")))
+  (add-to-list 'org-structure-template-alist '("py" . "src python")))
 
 ;; --- handy custom keys ---
 
