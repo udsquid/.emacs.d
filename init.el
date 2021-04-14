f65c6de26729d5c6c912280f1fd5b84da62001d9
neil.lin <neil.lin@ruckuswireless.com>
2021-04-14 10:43:53 +0800

    Add one org mode key-binding

--- init.el
+++ init.el
@@ -368,10 +368,11 @@
     "my" '(yank-rectangle                  :which-key "yank multiple")
 
     ;; org-mode
-    "o"  '(:ignore t      :which-key "org-mode")
-    "oc" '(org-capture    :which-key "capture")
-    "og" '(org-agenda     :which-key "agenda")
-    "ot" '(org-time-stamp :which-key "timestamp")
+    "o"  '(:ignore t                     :which-key "org-mode")
+    "oc" '(org-capture                   :which-key "capture")
+    "og" '(org-agenda                    :which-key "agenda")
+    "ot" '(org-time-stamp                :which-key "timestamp")
+    "ob" '(org-insert-structure-template :which-key "block")
 
     ;; org-roam
     "r"  '(:ignore t          :which-key "org-roam")
